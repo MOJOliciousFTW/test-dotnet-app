0dab9d9ebd57bec6cb48cf9ab54d9c9d981196d1
Johannes Møgster <johannes.mogster@mojotech.no>
2023-06-20 20:38:46 +0200

    use a private nuget package

---
+++ ProjectConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProjectClassLibrary1;
+
+var aClass = new Class1(firstVariable: 2.0, secondVariable: 2.0);
+var result = aClass.Add();
+
+Console.WriteLine("Hello, World! " + result);
